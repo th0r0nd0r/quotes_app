3f7b26ef3d4a5e832a1f06e64b3052ec48fad531
th0r0nd0r <pjfritch1@gmail.com>
2018-04-12 13:41:43 -0700

    add quotes controller methods

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,3 +1,32 @@
 class QuotesController < ApplicationController
-  
+  def new
+  end
+
+  def create
+    @quote = Quote.new(quote_params)
+
+    if @quote.save
+      render :show
+    else
+      # render json: @quote.errors.full_messages, status: 422
+    end
+  end
+
+  def index
+    @quotes = Quote.all
+
+    render :index
+  end
+
+  def show
+    @quote = Quote.includes(:customer).find(params[:id])
+
+    render :show
+  end
+
+
+  private
+  def quote_params
+    params.require(:quote).permit(:price, :priced, :customer_id)
+  end
 end
